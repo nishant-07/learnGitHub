112c3f34db7acd37b0d162f38c516505e0e2cae8
nishant-07 <nishantpraka@gmail.om>
2020-07-17 16:06:14 +0530

    commiting first.java

--- Gitlearn/src/test/java/firstCommit.java
+++ Gitlearn/src/test/java/firstCommit.java
@@ -1,8 +1,8 @@
 
 public class firstCommit {
 
-	public static void main(String args[]){
-		
+	public static void main(String args[]) {
+
 		System.out.println("First Commit...");
 		System.out.println("Pulling Git repo");
 		System.out.println("Pulling Git repo");
